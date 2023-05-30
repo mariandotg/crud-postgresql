4e31410972a84cb145ddf6dbcdde17d8dd651b79
Mariano Guillaume <marianguillaume.m@gmail.com>
2023-05-30 18:29:52 -0300

    Added DBManager

--- src/main/java/DBManager.java
+++ src/main/java/DBManager.java
@@ -2,13 +2,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class main {
-    public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
+public class DBManager {
+    private static final Dotenv dotenv = Dotenv.load();
 
-        String user = dotenv.get("DB_USER");
-        String password = dotenv.get("DB_PASSWORD");
+    private static final String user = dotenv.get("DB_USER");
+    private static final String password = dotenv.get("DB_PASSWORD");
 
+    public static void initConnection() {
         try {
             Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/Users", user, password);
             System.out.println("conectado con exito");
